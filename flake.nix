4b9ce6e8ba36559fe4bcc862d6f87aa8ae564d6a
Fajre <105254444+fajremvp@users.noreply.github.com>
2026-05-10 18:07:20 -0300

    feat: add homelab VM NixOS configuration

--- flake.nix
+++ flake.nix
@@ -37,6 +37,21 @@
           }
         ];
       };
+      homelab-vm = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./hosts/homelab-vm/default.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit inputs; };
+            home-manager.users.fajre = import ./users/fajre/home.nix;
+          }
+        ];
+      };
     };
   };
 }
---
+++ hosts/homelab-vm/default.nix
@@ -0,0 +1,34 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    # ./hardware.nix # <- Gerado automaticamente na instalação do NixOS.
+  ];
+
+  # Identidade da Máquina na Rede
+  networking.hostName = "homelab-nix";
+
+  # Localização e Fuso Horário
+  time.timeZone = "America/Sao_Paulo";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Habilitar Flakes nativamente no sistema
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # --- Configurações Gráficas e Wayland ---
+  # Habilita o suporte a gráficos (OpenGL/Mesa)
+  hardware.opengl.enable = true;
+  # Instala e configura o Niri no nível do SO.
+  programs.niri.enable = true;
+  # Essencial para gerenciadores de janelas rodarem bem sem um Desktop Environment
+  security.polkit.enable = true;
+
+  # Usuário Root / Sistema Base
+  users.users.fajre = {
+    isNormalUser = true;
+    extraGroups = [ "networkmanager" "wheel" "docker" ]; # wheel = sudo
+  };
+
+  # Versão do Estado do Sistema
+  system.stateVersion = "25.11";
+}
